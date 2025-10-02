f42fd05e4f390e863a8cc9ce4a062ef27584257f
ngoctan270905 <nn226509@gmail.com>
2025-10-02 18:57:12 +0700

    update DashboardController.php

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -1,13 +1,15 @@
-<?
+<?php
+
 namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+
 class DashboardController extends Controller
 {
     /**
      * Handle the incoming request.
      */
-    public function index()
+    public function __invoke()
     {
         return view('admin.dashboard');
     }
